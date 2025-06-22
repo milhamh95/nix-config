80a282f316f458d89a511746d7ce20010c44732d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-06-22 19:43:11 +0700

    feat: separate nix package

--- flake.nix
+++ flake.nix
@@ -32,42 +32,8 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
 
-      environment.systemPackages =
-        [
-          pkgs.atuin
-          pkgs.bat
-          pkgs.bun
-          pkgs.deno
-          pkgs.fastfetch
-          pkgs.fzf
-          pkgs.fishPlugins.forgit
-          pkgs.fishPlugins.tide
-          pkgs.fishPlugins.sponge
-          pkgs.fishPlugins.sdkman-for-fish
-          pkgs.fishPlugins.colored-man-pages
-          pkgs.fishPlugins.z
-          pkgs.fishPlugins.done
-          pkgs.go
-          pkgs.git
-          pkgs.lazygit
-          pkgs.lsd
-          pkgs.nodejs_22
-          pkgs.pnpm
-          pkgs.ripgrep
-          pkgs.skhd
-          pkgs.uv
-          pkgs.vim
-          pkgs.wget
-          pkgs.wifi-password
-          pkgs.yarn-berry_4
-        ];
-
-      fonts.packages = with pkgs; [
-        nerd-fonts.im-writing
-        nerd-fonts.hack
-        nerd-fonts.blex-mono
-        nerd-fonts.jetbrains-mono
-      ];
+      # Import system packages and fonts configuration
+      imports = [ ./nix-packages.nix ];
 
       # system.activationScripts.postActivation.text = ''
       #   su - "$(logname)" -c '${pkgs.skhd}/bin/skhd -r'
---
+++ nix-packages.nix
@@ -0,0 +1,42 @@
+# packages.nix - System packages and fonts configuration
+{ config, lib, pkgs, ... }:
+
+{
+  # System packages
+  environment.systemPackages = [
+    pkgs.atuin
+    pkgs.bat
+    pkgs.bun
+    pkgs.deno
+    pkgs.fastfetch
+    pkgs.fzf
+    pkgs.fishPlugins.forgit
+    pkgs.fishPlugins.tide
+    pkgs.fishPlugins.sponge
+    pkgs.fishPlugins.sdkman-for-fish
+    pkgs.fishPlugins.colored-man-pages
+    pkgs.fishPlugins.z
+    pkgs.fishPlugins.done
+    pkgs.go
+    pkgs.git
+    pkgs.lazygit
+    pkgs.lsd
+    pkgs.nodejs_22
+    pkgs.pnpm
+    pkgs.ripgrep
+    pkgs.skhd
+    pkgs.uv
+    pkgs.vim
+    pkgs.wget
+    pkgs.wifi-password
+    pkgs.yarn-berry_4
+  ];
+
+  # Font configuration
+  fonts.packages = with pkgs; [
+    nerd-fonts.im-writing
+    nerd-fonts.hack
+    nerd-fonts.blex-mono
+    nerd-fonts.jetbrains-mono
+  ];
+}
