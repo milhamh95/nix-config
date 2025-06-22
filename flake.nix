09614ad1ef0f65feae3add7b0ddd62febd208b3c
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-06-22 19:00:48 +0700

    feat: try separate homebrew nix

--- flake.nix
+++ flake.nix
@@ -116,82 +116,10 @@
           nixpkgs.config = lib.mkOrder 1500 (builtins.trace "🖥️  Building configuration for hostname: ${hostname}" {});
         })
 
-
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            user = "milhamh95";
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
-            };
-            autoMigrate = true;
-          };
-        }
-
-        {
-          homebrew = {
-            enable = true;
-            brews = [
-              "helix"
-              "mas"
-            ];
-            casks = [
-              "alt-tab"
-              "appcleaner"
-              "batfi"
-              "betterdisplay"
-              "bettermouse"
-              "brave-browser"
-              "discord"
-              "dotnet-sdk"
-              "elgato-stream-deck"
-              "flashspace"
-              "floorp"
-              "github"
-              "ghostty"
-              "google-chrome"
-              "hammerspoon"
-              "heptabase"
-              "homerow"
-              "iina"
-              "jordanbaird-ice"
-              "karabiner-elements"
-              "logitune"
-              "macupdater"
-              "microsoft-edge"
-              "orbstack"
-              "postman"
-              "postman@canary"
-              "pritunl"
-              "raycast"
-              "rectangle-pro"
-              "setapp"
-              "slack"
-              "visual-studio-code"
-              "vlc"
-              "wezterm"
-              "windsurf"
-              "zoom"
-            ];
-
-            masApps = {
-              "Amphetamine" = 937984704;
-              "DaisyDisk" = 411643860;
-              "Fantastical - Calendar" = 975937182;
-              "LilyView" = 529490330;
-              "PDF Expert – Edit, Sign PDFs" = 1055273043;
-              "rcmd • App Switcher" = 1596283165;
-              "Spark Classic – Email App" = 1176895641;
-            };
-
-            onActivation.cleanup = "zap";
-            onActivation.autoUpdate = true;
-            onActivation.upgrade = true;
-          };
-        }
+        # Import Homebrew configuration
+        (import ./homebrew.nix {
+          inherit homebrew-core homebrew-cask homebrew-bundle nix-homebrew;
+        })
 
         home-manager.darwinModules.home-manager
         {
