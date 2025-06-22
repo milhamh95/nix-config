7d7035423c2ecc1455fdbce69a6ee1729e87b283
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-06-22 19:32:47 +0700

    ref: separate homebrew config

--- flake.nix
+++ flake.nix
@@ -131,67 +131,8 @@
           };
         }
 
-        {
-          homebrew = {
-            enable = true;
-            brews = [
-              "helix"
-              "mas"
-            ];
-            casks = [
-              "alt-tab"
-              "appcleaner"
-              "batfi"
-              "betterdisplay"
-              "bettermouse"
-              "brave-browser"
-              "discord"
-              "dotnet-sdk"
-              "elgato-stream-deck"
-              "flashspace"
-              "floorp"
-              "github"
-              "ghostty"
-              "google-chrome"
-              "hammerspoon"
-              "heptabase"
-              "homerow"
-              "iina"
-              "jordanbaird-ice"
-              "karabiner-elements"
-              "logitune"
-              "macupdater"
-              "microsoft-edge"
-              "orbstack"
-              "postman"
-              "postman@canary"
-              "pritunl"
-              "raycast"
-              "rectangle-pro"
-              "setapp"
-              "slack"
-              "visual-studio-code"
-              "vlc"
-              "wezterm"
-              "windsurf"
-              "zoom"
-            ];
-
-            masApps = {
-              "Amphetamine" = 937984704;
-              "DaisyDisk" = 411643860;
-              "Fantastical - Calendar" = 975937182;
-              "LilyView" = 529490330;
-              "PDF Expert – Edit, Sign PDFs" = 1055273043;
-              "rcmd • App Switcher" = 1596283165;
-              "Spark Classic – Email App" = 1176895641;
-            };
-
-            onActivation.cleanup = "zap";
-            onActivation.autoUpdate = true;
-            onActivation.upgrade = true;
-          };
-        }
+        # Import Homebrew package configuration
+        ./homebrew.nix
 
         home-manager.darwinModules.home-manager
         {
---
+++ homebrew.nix
@@ -0,0 +1,64 @@
+# homebrew.nix - Homebrew package configuration
+{ config, lib, pkgs, ... }:
+
+{
+  homebrew = {
+    enable = true;
+    brews = [
+      "helix"
+      "mas"
+    ];
+    casks = [
+      "alt-tab"
+      "appcleaner"
+      "batfi"
+      "betterdisplay"
+      "bettermouse"
+      "brave-browser"
+      "discord"
+      "dotnet-sdk"
+      "elgato-stream-deck"
+      "flashspace"
+      "floorp"
+      "github"
+      "ghostty"
+      "google-chrome"
+      "hammerspoon"
+      "heptabase"
+      "homerow"
+      "iina"
+      "jordanbaird-ice"
+      "karabiner-elements"
+      "logitune"
+      "macupdater"
+      "microsoft-edge"
+      "orbstack"
+      "postman"
+      "postman@canary"
+      "pritunl"
+      "raycast"
+      "rectangle-pro"
+      "setapp"
+      "slack"
+      "visual-studio-code"
+      "vlc"
+      "wezterm"
+      "windsurf"
+      "zoom"
+    ];
+
+    masApps = {
+      "Amphetamine" = 937984704;
+      "DaisyDisk" = 411643860;
+      "Fantastical - Calendar" = 975937182;
+      "LilyView" = 529490330;
+      "PDF Expert – Edit, Sign PDFs" = 1055273043;
+      "rcmd • App Switcher" = 1596283165;
+      "Spark Classic – Email App" = 1176895641;
+    };
+
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+  };
+}
