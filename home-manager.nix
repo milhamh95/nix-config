0d4fe937fd8abf4282ef343294775ae05f64bf92
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-07-08 19:45:36 +0700

    feat: remove skhd

--- home-manager.nix
+++ home-manager.nix
@@ -204,13 +204,4 @@
   xdg.enable = true;
 
   imports = [ ./programs ];
-
-  services.skhd = {
-    enable = true;
-    package = pkgs.skhd;
-    config = ''
-      ctrl + shift + cmd - 9: flashspace profile Personal
-      ctrl + shift + cmd - 0: flashspace profile Work 
-    '';
-  };
 }
