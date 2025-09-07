ecce6931ca3647fe2582dd0bfc6c6faddc7bbde7
Muhammmad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-09-07 18:23:11 +0700

    fix: mise config location

--- home-manager.nix
+++ home-manager.nix
@@ -43,11 +43,11 @@
     configureMise = lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [ ! -d "$HOME/.mise" ]; then
         echo "Creating Mise directory... ⚙️"
-        $DRY_RUN_CMD mkdir -p "$HOME/.mise"
-        echo "Mise directory created at $HOME/.mise ✅"
+        $DRY_RUN_CMD mkdir -p "$HOME/.config/mise"
+        echo "Mise directory created at $HOME/.config/mise ✅"
 
         echo "Copying Mise config files..."
-        $DRY_RUN_CMD cp ${./app-config/mise/config.toml} "$HOME/.mise/config.toml"
+        $DRY_RUN_CMD cp ${./app-config/mise/config.toml} "$HOME/.config/mise/config.toml"
         echo "Mise configured ✅"
       fi
     '';
