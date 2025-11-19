edcd9bc82ea923dae3d08424edd9a6c4a6866bbf
Muhammmad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-11-19 20:32:58 +0700

    feat: use stable nix home manager

--- home-manager.nix
+++ home-manager.nix
@@ -1,5 +1,5 @@
 { hostname }: { config, pkgs, lib, ... }: {
-  home.stateVersion = "25.11";
+  home.stateVersion = "25.05";
 
   home.activation = {
     configureSsh = lib.hm.dag.entryAfter ["writeBoundary"] ''
