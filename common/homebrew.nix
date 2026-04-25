6c7bf9f5a4627504db18210117305875d668d8f5
Muhammmad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2026-04-25 07:30:29 +0700

    feat: disable auto update brew

--- common/homebrew.nix
+++ common/homebrew.nix
@@ -56,25 +56,27 @@
 
     # Mac App Store apps (shared across all machines)
     masApps = {
-      # "Amphetamine" = 937984704;
-      # "BarMarks" = 6739710035;
-      # "DaisyDisk" = 411643860;
-      # "ExcalidrawZ" = 6636493997;
-      # "Fantastical - Calendar" = 975937182;
-      # "iStat Menus" = 6499559693;
-      # "LilyView" = 529490330;
-      # "OpenIn" = 1643649331;
-      # "PastePal" = 1503446680;
-      # "PDF Expert – Edit, Sign PDFs" = 1055273043;
-      # "Presentify" = 1507246666;
-      # "rcmd • App Switcher" = 1596283165;
-      # "SnippetsLab" = 1006087419;
-      # "Spark Classic – Email App" = 1176895641;
-      # "Spokenly" = 6740315592;
+      "Amphetamine" = 937984704;
+      "BarMarks" = 6739710035;
+      "DaisyDisk" = 411643860;
+      "ExcalidrawZ" = 6636493997;
+      "Fantastical - Calendar" = 975937182;
+      "iStat Menus" = 6499559693;
+      "LilyView" = 529490330;
+      "OpenIn" = 1643649331;
+      "PastePal" = 1503446680;
+      "PDF Expert – Edit, Sign PDFs" = 1055273043;
+      "Presentify" = 1507246666;
+      "rcmd • App Switcher" = 1596283165;
+      "SnippetsLab" = 1006087419;
+      "Spark Classic – Email App" = 1176895641;
+      "Spokenly" = 6740315592;
     };
 
     onActivation.cleanup = "zap";
-    onActivation.autoUpdate = true;
+    # solve issue mas apps fail
+    # https://github.com/zhaofengli/nix-homebrew/issues/131#issuecomment-4275059685
+    onActivation.autoUpdate = false;
     onActivation.upgrade = true;
   };
 }
